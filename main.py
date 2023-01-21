83ab4aa6227d63b847fc5f39c5b993bbb94d7724
022by <46434255+pplulee@users.noreply.github.com>
2023-01-21 16:48:59 +0000

    增加自动检测更新

--- main.py
+++ main.py
@@ -9,15 +9,9 @@ import schedule
 web_url = ""
 web_key = ""
 
-
-def info(text):
-    print("[INFO] " + text)
-    logging.info(text)
-
-
-def error(text):
-    print("[ERROR] " + text)
-    logging.critical(text)
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 
 class local_docker:
@@ -25,24 +19,24 @@ class local_docker:
         self.local_list = self.get_local_list()
 
     def get_parameter(self, id):
-        info(f"获取容器{id}的参数")
+        logging.info(f"获取容器{id}的参数")
         try:
             result = requests.get(f"{web_url}/api/?action=get_parameter&key={web_key}&id={id}")
             result_json = json.loads(clean_html(result.text))
         except Exception as e:
-            error("获取API出错")
-            print(e)
+            logging.error("获取API出错")
+            logging.error(e)
             return None
         else:
             if result_json['status'] == "fail":
-                error(f"获取容器{id}的参数失败")
-                error(result_json['message'])
+                logging.error(f"获取容器{id}的参数失败")
+                logging.error(result_json['message'])
                 return None
         return result_json
 
     def deploy_docker(self, id):
         data = self.get_parameter(id)
-        info(f"部署容器{id}")
+        logging.info(f"部署容器{id}")
         password = data['password'].replace("$", "\$")
         os.system(f"docker run -d --name=autosign_{id} \
         -e username={data['username']} \
@@ -57,7 +51,7 @@ class local_docker:
         sahuidhsu/uom_autocheckin")
 
     def remove_docker(self, id):
-        info(f"删除容器{id}")
+        logging.info(f"删除容器{id}")
         os.system(f"docker stop autosign_{id} && docker rm autosign_{id}")
 
     def get_local_list(self):
@@ -66,7 +60,7 @@ class local_docker:
         for line in result.readlines():
             if line.find("autosign_") != -1:
                 local_list.append(line.strip().split("_")[1])
-        info(f"本地存在{len(local_list)}个容器")
+        logging.info(f"本地存在{len(local_list)}个容器")
         return local_list
 
     def get_remote_list(self):
@@ -74,22 +68,22 @@ class local_docker:
             result = requests.get(f"{web_url}/api/?action=get_list&key={web_key}")
             result_json = json.loads(clean_html(result.text))
         except Exception as e:
-            error("获取API出错")
+            logging.error("获取API出错")
             print(e)
             return self.local_list
         else:
             if result_json['status'] == "fail":
-                error(f"获取容器列表失败")
-                error(result_json['message'])
+                logging.error(f"获取容器列表失败")
+                logging.error(result_json['message'])
                 return self.local_list
         result_list = result_json['id_list'].split(",")
         if result_list == ['']:
             result_list = []
-        info(f"从云端获取到{len(result_list)}个容器")
+        logging.info(f"从云端获取到{len(result_list)}个容器")
         return result_list
 
     def sync(self):
-        info("开始同步")
+        logging.info("开始同步")
         self.local_list = self.get_local_list()
         # 处理需要删除的容器（本地存在，云端不存在）
         for id in self.local_list:
@@ -102,7 +96,26 @@ class local_docker:
             if id not in self.local_list:
                 self.deploy_docker(id)
                 self.local_list.append(id)
-        info("同步完成")
+        logging.info("同步完成")
+
+    def update(self):
+        logging.info("开始检查更新")
+        self.local_list = self.get_local_list()
+        if len(self.local_list) == 0:
+            logging.info("没有容器需要更新")
+            return
+        local_list_str = " ".join(self.local_list)
+        os.system(f"docker run --rm \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        containrrr/watchtower \
+        --cleanup \
+        --run-once \
+        {local_list_str}")
+
+
+def update():
+    global Local
+    Local.update()
 
 
 def clean_html(data):
@@ -113,16 +126,20 @@ def clean_html(data):
 
 
 def job():
-    info("开始定时任务")
+    logging.info("开始定时任务")
     Local.sync()
 
 
 def main():
-    info("自动签到后端服务启动")
+    logging.info("自动签到后端服务启动")
+    os.system("docker pull sahuidhsu/uom_autocheckin")
+    os.system("docker stop $(docker ps -a |  grep \"autosign*\"  | awk '{print $1}')")
+    os.system("docker rm $(docker ps -a |  grep \"autosign*\"  | awk '{print $1}')")
     global Local
     Local = local_docker()
     job()
     schedule.every(10).minutes.do(job)
+    schedule.every().day.at("00:00").do(update)
     while True:
         schedule.run_pending()
         time.sleep(1)
